4c3871b25b74db780e56bdd32451d45c5b6a38ba
Naraly <andrea.solis@sacmex.cdmx.gob.mx>
2023-11-10 14:10:55 -0600

    feat: db

--- simoti.sql
+++ simoti.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 15.3
 -- Dumped by pg_dump version 15.3
 
--- Started on 2023-11-10 14:09:04
+-- Started on 2023-11-10 14:10:36
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -41,6 +41,260 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+
+--
+-- TOC entry 227 (class 1255 OID 25114)
+-- Name: ejemplo(); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.ejemplo() RETURNS integer
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+ RETURN 104;
+END;
+$$;
+
+
+ALTER FUNCTION public.ejemplo() OWNER TO postgres;
+
+--
+-- TOC entry 228 (class 1255 OID 25117)
+-- Name: sp_verifyidentity(character varying, character varying); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.sp_verifyidentity(pusername character varying, ppassword character varying) RETURNS TABLE(id character varying, username character varying)
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+	return query
+ SELECT users.id, users.username
+	FROM users
+    WHERE 
+    users.username = pUsername 
+	AND users.password = pPassword;
+END;
+$$;
+
+
+ALTER FUNCTION public.sp_verifyidentity(pusername character varying, ppassword character varying) OWNER TO postgres;
+
+--
+-- TOC entry 226 (class 1259 OID 25112)
+-- Name: fall_detector_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.fall_detector_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.fall_detector_seq OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 225 (class 1259 OID 25105)
+-- Name: fall_detector; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.fall_detector (
+    id integer DEFAULT nextval('public.fall_detector_seq'::regclass) NOT NULL,
+    patient_id integer,
+    description character varying,
+    active boolean DEFAULT true,
+    created_at timestamp with time zone DEFAULT (now())::timestamp(0) without time zone
+);
+
+
+ALTER TABLE public.fall_detector OWNER TO postgres;
+
+--
+-- TOC entry 223 (class 1259 OID 25101)
+-- Name: heart_rate_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.heart_rate_seq
+    START WITH 5
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.heart_rate_seq OWNER TO postgres;
+
+--
+-- TOC entry 220 (class 1259 OID 25085)
+-- Name: heart_rate; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.heart_rate (
+    id integer DEFAULT nextval('public.heart_rate_seq'::regclass) NOT NULL,
+    patient_id integer,
+    value character varying,
+    active boolean DEFAULT true,
+    created_at timestamp with time zone DEFAULT (now())::timestamp(0) without time zone
+);
+
+
+ALTER TABLE public.heart_rate OWNER TO postgres;
+
+--
+-- TOC entry 222 (class 1259 OID 25099)
+-- Name: oxygen_saturation_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.oxygen_saturation_seq
+    START WITH 5
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.oxygen_saturation_seq OWNER TO postgres;
+
+--
+-- TOC entry 219 (class 1259 OID 25076)
+-- Name: oxygen_saturation; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.oxygen_saturation (
+    id integer DEFAULT nextval('public.oxygen_saturation_seq'::regclass) NOT NULL,
+    patient_id integer,
+    value character varying,
+    active boolean DEFAULT true,
+    created_at timestamp with time zone DEFAULT (now())::timestamp(0) without time zone
+);
+
+
+ALTER TABLE public.oxygen_saturation OWNER TO postgres;
+
+--
+-- TOC entry 214 (class 1259 OID 16952)
+-- Name: permissions; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.permissions (
+    id character varying NOT NULL,
+    permission character varying NOT NULL,
+    description character varying NOT NULL,
+    active boolean DEFAULT true NOT NULL,
+    created_at timestamp(6) without time zone DEFAULT now() NOT NULL,
+    created_by integer DEFAULT 1 NOT NULL,
+    updated_at timestamp(6) without time zone DEFAULT now() NOT NULL,
+    updated_by integer DEFAULT 1 NOT NULL
+);
+
+
+ALTER TABLE public.permissions OWNER TO postgres;
+
+--
+-- TOC entry 217 (class 1259 OID 17010)
+-- Name: role_has_permissions; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.role_has_permissions (
+    role_id character varying NOT NULL,
+    permission_id character varying NOT NULL
+);
+
+
+ALTER TABLE public.role_has_permissions OWNER TO postgres;
+
+--
+-- TOC entry 216 (class 1259 OID 17000)
+-- Name: roles; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.roles (
+    id character varying NOT NULL,
+    active boolean DEFAULT true NOT NULL,
+    role character varying NOT NULL,
+    created_at timestamp(6) without time zone NOT NULL,
+    created_by character varying NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL,
+    updated_by character varying NOT NULL
+);
+
+
+ALTER TABLE public.roles OWNER TO postgres;
+
+--
+-- TOC entry 215 (class 1259 OID 16966)
+-- Name: roles_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.roles_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.roles_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 224 (class 1259 OID 25103)
+-- Name: temperature_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.temperature_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.temperature_seq OWNER TO postgres;
+
+--
+-- TOC entry 221 (class 1259 OID 25092)
+-- Name: temperature; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.temperature (
+    id integer DEFAULT nextval('public.temperature_seq'::regclass) NOT NULL,
+    patient_id integer,
+    value character varying,
+    active boolean DEFAULT true,
+    created_at timestamp with time zone DEFAULT (now())::timestamp(0) without time zone
+);
+
+
+ALTER TABLE public.temperature OWNER TO postgres;
+
+--
+-- TOC entry 218 (class 1259 OID 17045)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id character varying NOT NULL,
+    active boolean DEFAULT true NOT NULL,
+    created_at timestamp(6) without time zone DEFAULT now() NOT NULL,
+    created_by character varying DEFAULT 1 NOT NULL,
+    last_name character varying NOT NULL,
+    middle_name character varying NOT NULL,
+    first_name character varying NOT NULL,
+    password character varying NOT NULL,
+    role_id character varying NOT NULL,
+    updated_at timestamp(6) without time zone DEFAULT now() NOT NULL,
+    updated_by character varying DEFAULT 1 NOT NULL,
+    username character varying NOT NULL,
+    profile_pic_url character varying
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
 
 --
 -- TOC entry 3400 (class 0 OID 25105)
@@ -211,7 +465,97 @@ SELECT pg_catalog.setval('public.roles_id_seq', 1, false);
 SELECT pg_catalog.setval('public.temperature_seq', 5, true);
 
 
--- Completed on 2023-11-10 14:09:05
+--
+-- TOC entry 3237 (class 2606 OID 17016)
+-- Name: role_has_permissions PK_9271906febebfc15e39b49fa738; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.role_has_permissions
+    ADD CONSTRAINT "PK_9271906febebfc15e39b49fa738" PRIMARY KEY (role_id, permission_id);
+
+
+--
+-- TOC entry 3239 (class 2606 OID 17054)
+-- Name: users PK_a3ffb1c0c8416b9fc6f907b7433; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3233 (class 2606 OID 17009)
+-- Name: roles PK_c1433d71a4838793a49dcad46ab; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.roles
+    ADD CONSTRAINT "PK_c1433d71a4838793a49dcad46ab" PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3235 (class 2606 OID 17007)
+-- Name: roles UQ_ccc7c1489f3a6b3c9b47d4537c5; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.roles
+    ADD CONSTRAINT "UQ_ccc7c1489f3a6b3c9b47d4537c5" UNIQUE (role);
+
+
+--
+-- TOC entry 3241 (class 2606 OID 17052)
+-- Name: users UQ_fe0bb3f6520ee0469504521e710; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE (username);
+
+
+--
+-- TOC entry 3243 (class 2606 OID 25084)
+-- Name: oxygen_saturation oxygen_saturation_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.oxygen_saturation
+    ADD CONSTRAINT oxygen_saturation_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3231 (class 2606 OID 17028)
+-- Name: permissions permissions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.permissions
+    ADD CONSTRAINT permissions_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3246 (class 2606 OID 17055)
+-- Name: users FK_a2cecd1a3531c0b041e29ba46e1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT "FK_a2cecd1a3531c0b041e29ba46e1" FOREIGN KEY (role_id) REFERENCES public.roles(id);
+
+
+--
+-- TOC entry 3244 (class 2606 OID 25066)
+-- Name: role_has_permissions fk_role_has_permissions_permissions; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.role_has_permissions
+    ADD CONSTRAINT fk_role_has_permissions_permissions FOREIGN KEY (permission_id) REFERENCES public.permissions(id) ON UPDATE CASCADE ON DELETE CASCADE NOT VALID;
+
+
+--
+-- TOC entry 3245 (class 2606 OID 25071)
+-- Name: role_has_permissions fk_role_has_permissions_roles; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.role_has_permissions
+    ADD CONSTRAINT fk_role_has_permissions_roles FOREIGN KEY (role_id) REFERENCES public.roles(id) ON UPDATE CASCADE ON DELETE CASCADE NOT VALID;
+
+
+-- Completed on 2023-11-10 14:10:36
 
 --
 -- PostgreSQL database dump complete
