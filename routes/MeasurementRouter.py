524d7a9875ca6fa11a1120395c7d63d0655007f7
Andrea <naralysolis@hotmail.com>
2024-01-06 12:25:19 -0600

    feat: heart rate add

--- routes/MeasurementRouter.py
+++ routes/MeasurementRouter.py
@@ -116,6 +116,7 @@ def get_measurements_heart_rate_by_patient(patient_id):
 @main.route("/heartRate/add", methods=["POST"])
 def get_measurements_heart_rate_add():
     try:
+        print('request',request)
         value = request.json["value"]
         patient_id = request.json["patient_id"]
         rate = heartRate(value, patient_id)
