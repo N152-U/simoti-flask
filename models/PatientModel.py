bddc1e22fbafd156362ab0afc847874dc40814b2
Naraly Solis <naralysolis@outlook.com>
2025-05-20 23:12:58 -0600

    feat: patients

--- models/PatientModel.py
+++ models/PatientModel.py
@@ -7,7 +7,7 @@ from .entities.Patient import Patient
 class PatientModel:
 
     @classmethod
-    def add_patient(self, patient):
+    def add_patient(self, patient,patientGeneralStatus):
         try:
             connection = get_connection()
 
@@ -26,6 +26,32 @@ class PatientModel:
                         patient.date_of_birth
                     ),
                 )
+                
+                cursor.execute(
+                    """INSERT INTO patient_general_status (patient_id, general_condition, energy, fever, chest_pain, dizziness, high_temperature, sweating, 
+                 palpitations, resting_tachycardia, falls, instability, change_of_location, exercise_difficulty, dyspnea_activities) 
+                                VALUES (%s, %s, %s,%s, %s, %s, %s,%s, %s, %s,%s, %s, %s, %s, %s)""",
+                    (
+                        patient.id,
+                        True if patientGeneralStatus.general_condition == 1 else False,
+                        True if patientGeneralStatus.energy == 1 else False,
+                        True if patientGeneralStatus.fever == 1 else False,
+                        True if patientGeneralStatus.chest_pain == 1 else False,
+                        True if patientGeneralStatus.dizziness == 1 else False,
+                        True if patientGeneralStatus.high_temperature == 1 else False,
+                        True if patientGeneralStatus.sweating == 1 else False,
+                        True if patientGeneralStatus.palpitations == 1 else False,
+                        True if patientGeneralStatus.resting_tachycardia == 1 else False,
+                        True if patientGeneralStatus.falls == 1 else False,
+                        True if patientGeneralStatus.instability == 1 else False,
+                        True if patientGeneralStatus.change_of_location == 1 else False,
+                        True if patientGeneralStatus.exercise_difficulty == 1 else False,
+                        True if patientGeneralStatus.dyspnea_activities == 1 else False
+
+                    ),
+                )
+                
+                
                 affected_rows = cursor.rowcount
                 connection.commit()
 
--- routes/PatientRouter.py
+++ routes/PatientRouter.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 import uuid
 from werkzeug.security import generate_password_hash as genph
 # Entities
-from models.entities.Patient import Patient
+from models.entities.Patient import Patient, PatientGeneralStatus, PatientHabitsAndBackgroud
 # Security
 from utils.Security import Security
 
@@ -27,15 +27,35 @@ def add_user():
             tutor_id = request.json["tutor_id"]
             doctor_id = request.json["doctor_id"]
             date_of_birth = request.json["date_of_birth"]
+            
+            patient_id=id
+            general_condition=int(request.json["estadoGeneral"])
+            energy=int(request.json["energia"])
+            fever=int(request.json["fiebre"])
+            chest_pain=int(request.json["dolorPecho"])
+            dizziness=int(request.json["mareos"])
+            high_temperature=int(request.json["tempAlta"])
+            sweating=int(request.json["sudores"])
+            palpitations=int(request.json["palpitaciones"])
+            resting_tachycardia=int(request.json["taquicardiaReposo"])
+            falls=int(request.json["caidas"])
+            instability=int(request.json["inestabilidad"])
+            change_of_location=int(request.json["cambioLugar"])
+            exercise_difficulty=int(request.json["dificultadEjercicio"])
+            dyspnea_activities=int(request.json["disneaActividades"])
 
             newPatient =  Patient(
                 str(id), True, first_name, middle_name, last_name, tutor_id, doctor_id, date_of_birth
             )
+            
+            newPatientGeneralStatus = PatientGeneralStatus(patient_id, general_condition, energy, fever, chest_pain, dizziness, high_temperature, sweating, 
+                 palpitations, resting_tachycardia, falls, instability, change_of_location, exercise_difficulty, dyspnea_activities)
+            #newPatientHabitsAndBackgroud = PatientHabitsAndBackgroud()
 
-            affected_rows = PatientModel.add_patient(newPatient)
+            affected_rows = PatientModel.add_patient(newPatient,newPatientGeneralStatus)
 
             if affected_rows == 1:
-                return jsonify(newPatient.id),202
+                return jsonify(newPatient.id),201
 
             else:
                 return jsonify({"message": "Error on insert"}), 500
